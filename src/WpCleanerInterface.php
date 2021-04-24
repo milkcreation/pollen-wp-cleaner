3083aec2128150b74330ccfba4d74dd2f5f3c923
Jordy Manner <jordy.manner@milkcreation.fr>
2021-04-24 12:17:46 +0000

    wp-cleaner >> 1.1.x-dev

--- src/WpCleanerInterface.php
+++ src/WpCleanerInterface.php
@@ -6,11 +6,13 @@ namespace Pollen\WpCleaner;
 
 use Pollen\Support\Concerns\BootableTraitInterface;
 use Pollen\Support\Concerns\ConfigBagAwareTraitInterface;
+use Pollen\Support\Concerns\ResourcesAwareTraitInterface;
 use Pollen\Support\Proxy\ContainerProxyInterface;
 
 interface WpCleanerInterface extends
     BootableTraitInterface,
     ConfigBagAwareTraitInterface,
+    ResourcesAwareTraitInterface,
     ContainerProxyInterface
 {
     /**
@@ -19,22 +21,4 @@ interface WpCleanerInterface extends
      * @return static
      */
     public function boot(): WpCleanerInterface;
-
-    /**
-     * Chemin absolu vers une ressources (fichier|répertoire).
-     *
-     * @param string|null $path Chemin relatif vers la ressource.
-     *
-     * @return string
-     */
-    public function resources(?string $path = null): string;
-
-    /**
-     * Définition du chemin absolu vers le répertoire des ressources.
-     *
-     * @var string $resourceBaseDir
-     *
-     * @return static
-     */
-    public function setResourcesBaseDir(string $resourceBaseDir): WpCleanerInterface;
 }
